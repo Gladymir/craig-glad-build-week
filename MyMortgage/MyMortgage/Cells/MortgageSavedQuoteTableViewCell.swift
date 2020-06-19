75ff03b6862acb17e51172f013e12fa8d280bdc8
Gladymir <gaethan0815@gmail.com>
2020-06-18 19:34:09 -0600

    Home

---
+++ MyMortgage/MyMortgage/Cells/MortgageSavedQuoteTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MortgageSavedQuoteTableViewCell.swift
+//  MyMortgage
+//
+//  Created by Gladymir Philippe on 6/18/20.
+//  Copyright © 2020 Gladymir Philippe. All rights reserved.
+//
+
+import UIKit
+
+class MortgageSavedQuoteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
